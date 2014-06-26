fb4aadbe9707396d91c6b8074296eef4d90cdd7c
Rahul Agarwal <rahul_agarwal@intuit.com>
2014-06-26 10:58:02 -0700

    Adding local time test

--- src/test/java/com/irahul/java8/time/TestLocalDateTime.java
+++ src/test/java/com/irahul/java8/time/TestLocalDateTime.java
@@ -1,7 +1,10 @@
 package com.irahul.java8.time;
 
+import java.time.DateTimeException;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.Month;
 import java.time.temporal.ChronoField;
 import java.time.temporal.TemporalAdjusters;
@@ -10,12 +13,15 @@ import org.junit.Assert;
 import org.junit.Test;
 
 /**
- * Local date - no time info available. This date is "local" and valid no matter where in the world
+ * These are "local" times with no timezone info
  * @author rahul
  *
  */
-public class TestLocalDate {
+public class TestLocalDateTime {
 
+	/**
+	 * Local date - no time info available. This date is "local" and valid no matter where in the world
+	 */
 	@Test
 	public void testLocalDate(){
 		//today
@@ -52,5 +58,40 @@ public class TestLocalDate {
 		LocalDate memorialDay = may2014.with(TemporalAdjusters.lastInMonth(DayOfWeek.MONDAY));
 		Assert.assertEquals(26, memorialDay.getDayOfMonth());
 		Assert.assertEquals(5, memorialDay.getMonthValue());		
+	}
+	
+	/**
+	 * Only contains time info, no date or timezone. "Local" for any place in the world
+	 */
+	@Test
+	public void testLocalTime(){
+		LocalTime now = LocalTime.now();
+		System.out.println(String.format("toString=%s", now.toString()));
+		
+		LocalTime midnight = LocalTime.of(0, 0);
+		Assert.assertEquals(0, midnight.getHour());
+		Assert.assertEquals(0, midnight.getMinute());
+		
+		LocalTime pm1 = LocalTime.of(13, 25, 59);
+		Assert.assertEquals(13, pm1.getHour());
+		Assert.assertEquals(25, pm1.getMinute());
+		Assert.assertEquals(59, pm1.getSecond());
+		
+		try{
+			LocalTime.of(25, 0);//allowed 0 to 23
+			Assert.fail();
+		}
+		catch (DateTimeException e){
+			//expected
+		}				
+	}
+	
+	/**
+	 * Combines above info
+	 */
+	@Test
+	public void testLocalDateTime(){
+		LocalDateTime now = LocalDateTime.now();
+		System.out.println(String.format("toString=%s", now.toString()));
 	}
 }
